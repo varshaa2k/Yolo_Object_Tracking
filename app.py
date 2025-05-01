5e4fddb34a774756cfafce47eb9b058d8f839dce
varshaa2k <67535245+varshaa2k@users.noreply.github.com>
2025-05-01 15:24:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,6 @@ if __name__ == '__main__':
     # Create upload folder if it doesn't exist
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-        
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))    
+    app.run(host="0.0.0.0", port=port)
 
